96b75a2cce40b5ffed3999ec663b77e79efa10ee
jacquelyn-bradshaw <home@bradshaw.local>
2024-01-14 20:18:16 +0000

    user defined function to determine farmers needed per field

---
+++ user_defined_function.sql
@@ -0,0 +1,22 @@
+USE farmManager;
+
+/* A stored function that accepts field size as a parameter and
+then assesses whether 1 or 2 farmers are needed.
+*/
+DELIMITER //
+CREATE FUNCTION number_of_farmers_needed(size INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE number_of_farmers INT;
+    IF size <= 10 THEN SET number_of_farmers = 1;
+    ELSEIF size > 10 THEN SET number_of_farmers = 2;
+    END IF;
+    RETURN number_of_farmers;
+    END //
+DELIMITER ;
+
+/* Use the number_of_farmers_needed function on the field
+table to show how many farmers each field needs */
+SELECT name, size, number_of_farmers_needed(size)
+FROM field;
