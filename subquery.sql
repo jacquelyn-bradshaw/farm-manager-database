fad4ad37cb7998ba9976ff003cfb440f90e14025
jacquelyn-bradshaw <home@bradshaw.local>
2024-01-17 09:56:21 +0000

    tables renamed

--- subquery.sql
+++ subquery.sql
@@ -2,9 +2,9 @@ USE farmManager;
 
 /* Query with subquery to find the fields that have the
 crop wheat planted */
-SELECT name
-FROM field
+SELECT name AS "Field Name"
+FROM fields
 WHERE cropID IN (
-  SELECT cropID FROM crop
+  SELECT cropID FROM crops
   WHERE type="wheat"
 );
